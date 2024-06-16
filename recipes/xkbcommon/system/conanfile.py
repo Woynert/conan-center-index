b37ead5bd59a8747dbe22eecebfb85b1876ed7df
woynert <woynertgamer@gmail.com>
2024-06-16 08:09:02 -0500

    feat(xkbcommon): support system provided version

---
+++ recipes/xkbcommon/system/conanfile.py
@@ -0,0 +1,48 @@
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.gnu import PkgConfig
+from conan.tools.system import package_manager
+
+required_conan_version = ">=1.50.0"
+
+
+class ConanGTK(ConanFile):
+    name = "xkbcommon"
+    description = "keymap handling library for toolkits and window systems"
+    topics = ("keyboard", "wayland", "x11", "xkb")
+    url = "https://github.com/conan-io/conan-center-index"
+    homepage = "https://github.com/xkbcommon/libxkbcommon"
+    license = "MIT"
+    package_type = "shared-library"
+    settings = "os", "arch", "compiler", "build_type"
+
+    def package_id(self):
+        self.info.settings.clear()
+
+    def validate(self):
+        if self.settings.os not in ["Linux", "FreeBSD"]:
+            raise ConanInvalidConfiguration("This recipe supports only Linux and FreeBSD")
+
+    # TODO
+    # def system_requirements(self):
+        # dnf = package_manager.Dnf(self)
+        # dnf.install([f"gtk{self.options.version}-devel"], update=True, check=True)
+
+        # yum = package_manager.Yum(self)
+        # yum.install([f"gtk{self.options.version}-devel"], update=True, check=True)
+
+        # apt = package_manager.Apt(self)
+        # apt.install(["libgtk2.0-dev"] if self.options.version == 2 else ["libgtk-3-dev"], update=True, check=True)
+
+        # pacman = package_manager.PacMan(self)
+        # pacman.install([f"gtk{self.options.version}"], update=True, check=True)
+
+        # zypper = package_manager.Zypper(self)
+        # zypper.install([f"gtk{self.options.version}-devel"], update=True, check=True)
+
+        # pkg = package_manager.Pkg(self)
+        # pkg.install([f"gtk{self.options.version}"], update=True, check=True)
+
+    def package_info(self):
+        pkg_config = PkgConfig(self, "xkbcommon")
+        pkg_config.fill_cpp_info(self.cpp_info, is_system=True)
